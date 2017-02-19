ef8f07d77bc212d8da7d6b277caf887a96189886
Thomas Conner <thomas@kinvey.com>
2017-02-19 09:25:44 -0500

    MLIBZ-1542 fix issues with push notifications

--- src/push.js
+++ src/push.js
@@ -4,14 +4,18 @@ import {
   CacheRequest,
   KinveyRequest,
   User,
-  Client
+  Client,
+  isDefined,
+  NotFoundError
 } from 'kinvey-node-sdk/dist/export';
 import { EventEmitter } from 'events';
 import Device from './device';
 import url from 'url';
 import bind from 'lodash/bind';
-const pushNamespace = process.env.KINVEY_PUSH_NAMESPACE || 'push';
-const notificationEvent = process.env.KINVEY_NOTIFICATION_EVENT || 'notification';
+const APP_DATA_NAMESPACE = process.env.KINVEY_DATASTORE_NAMESPACE || 'appdata';
+const PUSH_NAMESPACE = process.env.KINVEY_PUSH_NAMESPACE || 'push';
+const NOTIFICATION_EVENT = process.env.KINVEY_NOTIFICATION_EVENT || 'notification';
+const DEVICE_COLLECTION = '__device';
 let CloudPush;
 
 if (Device.isAndroid()) {
@@ -21,7 +25,7 @@ if (Device.isAndroid()) {
 
 class Push extends EventEmitter {
   get pathname() {
-    return `/${pushNamespace}/${this.client.appKey}`;
+    return `/${PUSH_NAMESPACE}/${this.client.appKey}`;
   }
 
   get client() {
@@ -45,11 +49,11 @@ class Push extends EventEmitter {
   }
 
   onNotification(listener) {
-    return this.on(notificationEvent, listener);
+    return this.on(NOTIFICATION_EVENT, listener);
   }
 
   onceNotification(listener) {
-    return this.once(notificationEvent, listener);
+    return this.once(NOTIFICATION_EVENT, listener);
   }
 
   register(options = {}) {
@@ -59,93 +63,90 @@ class Push extends EventEmitter {
       );
     }
 
-    return this.unregister()
-      .catch(() => null)
-      .then(() => {
-        return new Promise((resolve, reject) => {
-          const notificationListener = bind((data) => {
-            this.emit(notificationEvent, data);
-          }, this);
+    return new Promise((resolve, reject) => {
+      const notificationListener = bind((data) => {
+        this.emit(NOTIFICATION_EVENT, data);
+      }, this);
 
-          if (Device.isiOS()) {
-            if (parseInt(Ti.Platform.version.split('.')[0], 10) >= 8) {
-              Ti.App.iOS.addEventListener('usernotificationsettings', function registerForPush() {
-                Ti.App.iOS.removeEventListener('usernotificationsettings', registerForPush);
-                Ti.Network.registerForPushNotifications({
-                  success(e) {
-                    resolve(e.deviceToken);
-                  },
-                  error(e) {
-                    reject(new Error('An error occurred registering this device'
-                      + ' for push notifications.', e));
-                  },
-                  callback: notificationListener
-                });
-              });
-
-              const types = [];
-
-              if (options.ios) {
-                if (options.ios.alert) {
-                  types.push(Ti.App.iOS.USER_NOTIFICATION_TYPE_ALERT);
-                }
-
-                if (options.ios.badge) {
-                  types.push(Ti.App.iOS.USER_NOTIFICATION_TYPE_BADGE);
-                }
-
-                if (options.ios.sound) {
-                  types.push(Ti.App.iOS.USER_NOTIFICATION_TYPE_SOUND);
-                }
-              }
-
-              Ti.App.iOS.registerUserNotificationSettings({
-                types: types,
-                categories: options.categories || []
-              });
-            } else {
-              const types = [];
-
-              if (options.ios) {
-                if (options.ios.alert) {
-                  types.push(Ti.Network.NOTIFICATION_TYPE_ALERT);
-                }
-
-                if (options.ios.badge) {
-                  types.push(Ti.Network.NOTIFICATION_TYPE_BADGE);
-                }
-
-                if (options.ios.sound) {
-                  types.push(Ti.Network.NOTIFICATION_TYPE_SOUND);
-                }
-              }
-
-              Ti.Network.registerForPushNotifications({
-                types: types,
-                success(e) {
-                  resolve(e.deviceToken);
-                },
-                error(e) {
-                  reject(new Error('An error occurred registering this device for push notifications.', e));
-                },
-                callback: notificationListener
-              });
-            }
-          } else if (Device.isAndroid()) {
-            CloudPush.retrieveDeviceToken({
+      if (Device.isiOS()) {
+        if (parseInt(Ti.Platform.version.split('.')[0], 10) >= 8) {
+          Ti.App.iOS.addEventListener('usernotificationsettings', function registerForPush() {
+            Ti.App.iOS.removeEventListener('usernotificationsettings', registerForPush);
+            Ti.Network.registerForPushNotifications({
               success(e) {
                 resolve(e.deviceToken);
               },
               error(e) {
-                reject(new Error('An error occurred registering this device for'
-                  + ' push notifications.', e));
-              }
+                reject(new Error('An error occurred registering this device'
+                  + ' for push notifications.', e));
+              },
+              callback: notificationListener
             });
+          });
 
-            CloudPush.addEventListener('callback', notificationListener);
+          const types = [];
+
+          if (options.ios) {
+            if (options.ios.alert) {
+              types.push(Ti.App.iOS.USER_NOTIFICATION_TYPE_ALERT);
+            }
+
+            if (options.ios.badge) {
+              types.push(Ti.App.iOS.USER_NOTIFICATION_TYPE_BADGE);
+            }
+
+            if (options.ios.sound) {
+              types.push(Ti.App.iOS.USER_NOTIFICATION_TYPE_SOUND);
+            }
+          }
+
+          Ti.App.iOS.registerUserNotificationSettings({
+            types: types,
+            categories: options.categories || []
+          });
+        } else {
+          const types = [];
+
+          if (options.ios) {
+            if (options.ios.alert) {
+              types.push(Ti.Network.NOTIFICATION_TYPE_ALERT);
+            }
+
+            if (options.ios.badge) {
+              types.push(Ti.Network.NOTIFICATION_TYPE_BADGE);
+            }
+
+            if (options.ios.sound) {
+              types.push(Ti.Network.NOTIFICATION_TYPE_SOUND);
+            }
+          }
+
+          Ti.Network.registerForPushNotifications({
+            types: types,
+            success(e) {
+              resolve(e.deviceToken);
+            },
+            error(e) {
+              reject(new Error('An error occurred registering this device for push notifications.', e));
+            },
+            callback: notificationListener
+          });
+        }
+      } else if (Device.isAndroid()) {
+        CloudPush.setShowTrayNotificationsWhenFocused(true);
+        CloudPush.retrieveDeviceToken({
+          success(e) {
+            resolve(e.deviceToken);
+          },
+          error(e) {
+            reject(new Error('An error occurred registering this device for'
+              + ' push notifications.', e.error));
           }
         });
-      })
+
+        CloudPush.addEventListener('callback', notificationListener);
+      }
+    })
       .then((deviceId) => {
         if (typeof deviceId === 'undefined') {
           throw new Error('Unable to retrieve the device id to register this device for push notifications.');
@@ -171,23 +172,31 @@ class Push extends EventEmitter {
           client: this.client
         });
         return request.execute()
-          .then(response => response.data)
-          .then((data) => {
-            const request = new CacheRequest({
-              method: RequestMethod.PUT,
-              url: url.format({
-                protocol: this.client.protocol,
-                host: this.client.host,
-                pathname: `${this.pathname}/device`
-              }),
-              data: {
-                deviceId: deviceId
-              },
-              client: this.client
-            });
-            return request.execute()
-              .then(() => data);
-          });
+          .then(() => deviceId);
+      })
+      .then((deviceId) => {
+        const user = User.getActiveUser(this.client);
+        let _id = options.userId;
+
+        if (isDefined(user)) {
+          _id = user._id;
+        }
+
+        const request = new CacheRequest({
+          method: RequestMethod.PUT,
+          url: url.format({
+            protocol: this.client.protocol,
+            host: this.client.host,
+            pathname: `/${APP_DATA_NAMESPACE}/${this.client.appKey}/${DEVICE_COLLECTION}`
+          }),
+          data: {
+            _id: _id,
+            deviceId: deviceId
+          },
+          client: this.client
+        });
+        return request.execute()
+          .then(() => deviceId);
       });
   }
 
@@ -209,12 +218,18 @@ class Push extends EventEmitter {
     });
     return request.execute()
       .then(response => response.data)
-      .then(({ deviceId }) => {
-        if (typeof deviceId === 'undefined') {
-          throw new Error('This device has not been registered for push notifications.');
+      .then((device) => {
+        const user = User.getActiveUser(this.client);
+        let deviceId;
+
+        if (isDefined(device)) {
+          deviceId = device.deviceId;
         }
 
-        const user = User.getActiveUser(this.client);
+        if (isDefined(deviceId) === false) {
+          return null;
+        }
+
         const request = new KinveyRequest({
           method: RequestMethod.POST,
           url: url.format({
@@ -236,18 +251,33 @@ class Push extends EventEmitter {
         return request.execute()
           .then(response => response.data);
       })
-      .then((data) => {
+      .then(() => {
+        const user = User.getActiveUser(this.client);
+        let _id = options.userId;
+
+        if (isDefined(user)) {
+          _id = user._id;
+        }
+
         const request = new CacheRequest({
           method: RequestMethod.DELETE,
           url: url.format({
             protocol: this.client.protocol,
             host: this.client.host,
-            pathname: `${this.pathname}/device`
+            pathname: `/${APP_DATA_NAMESPACE}/${this.client.appKey}/${DEVICE_COLLECTION}/${_id}`
           }),
           client: this.client
         });
+
         return request.execute()
-          .then(() => data);
+          .catch((error) => {
+            if (error instanceof NotFoundError) {
+              return {};
+            }
+
+            throw error;
+          })
+          .then(() => null);
       });
   }
 }
